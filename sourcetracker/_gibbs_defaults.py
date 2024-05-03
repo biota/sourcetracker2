c56abe4fe62342dc8bf96d38507f9b033b1c73dd
andy6a <128522580+andy6a@users.noreply.github.com>
2024-05-03 13:02:40 -0700

    Update _gibbs_defaults.py

--- sourcetracker/_gibbs_defaults.py
+++ sourcetracker/_gibbs_defaults.py
@@ -117,7 +117,8 @@ DESC_HCOL = ('Heatmap coloring. Coloring pattern for default and'
              ' paired heatmap. Default is viridis, other options in'
              ' plot.py.')
 DESC_UKN = ('keep unknown sources in heatmap or stacked bar plots.'
-            'IMPORTANT: Setting to False will normalize proportions back to 1.')
+            'IMPORTANT: Setting to False will normalize proportions back to'
+            '1.')
 DESC_TRA = ('Transpose Heatmap plots.Flips x and y axis. Default is'
             ' False.')
 DESC_BCOL = ('Coloring for stacked bar plot. Default is matplotlib'
